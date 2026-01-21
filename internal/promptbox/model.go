71b8467c6ee727f6a5de22a65bc14e96a6f702bc
Brandon C. Irizarry <brandon.irizarry@gmail.com>
2026-01-20 21:06:44 -0500

    refactor: move GetPrompt to model.go
    This way, there is now only one file inside the promptbox package.

--- internal/promptbox/model.go
+++ internal/promptbox/model.go
@@ -62,3 +62,26 @@ func (m model) View() string {
 
 	return view.String()
 }
+
+)
+
+func GetPrompt() (string, error) {
+	txt := textarea.New()
+	txt.Placeholder = "Write your prompt here..."
+	txt.ShowLineNumbers = false
+
+	// Don't set a background highlighting color, since at least
+	// in Vterm under an Emacs light theme this will still paint
+	// the background black (lipgloss thinks the terminal is using
+	// a dark theme somehow.)
+	txt.FocusedStyle.CursorLine = lipgloss.NewStyle().Background(lipgloss.Color(""))
+	p := tea.NewProgram(model{textarea: txt})
+
+	m, err := p.Run()
+	if err != nil {
+		return "", err
+	}
+
+	pbox := m.(model)
+	return pbox.promptText, nil
+}
